cc9ccb64f521140ca468a833cc1a30323ac2d4c3
Dan Kempster <dev@dankempster.co.uk>
2015-03-25 19:26:59 +0000

    Fix: Load YAML for DI service definitions

--- DependencyInjection/AxstradExtraFrameworkExtension.php
+++ DependencyInjection/AxstradExtraFrameworkExtension.php
@@ -38,8 +38,8 @@ class AxstradExtraFrameworkExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 
         foreach ($config['class'] as $purpose => $class) {
             $container->setParameter(sprintf('axstrad_extra_framework.class.%s', $purpose), $class);
